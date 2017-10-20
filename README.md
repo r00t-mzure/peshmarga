19c0a8c6f3e907b75ad6ecb066121ac1f02ee708
r00t-mzure <32970330+r00t-mzure@users.noreply.github.com>
2017-10-21 01:57:46 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+<html>
+<body>
+<h1 align="center">Peshmarga</h1>
+ <p> This is a project for Encryption Web Shell
+</p>
+
+<table border="0" cellpadding="2" cellspacing="5" width="100%">
+  <tr>
+    <td class="main3" width="890px"><b>Download:</b></td>
+  </tr>
+  <tr>
+    <td class="main">
+      git clone https://github.com/r00t-mzure/peshmarga.git <br/>
+      OR direct link: https://github.com/r00t-mzure/peshmarga.git
+    </td>
+  </tr>
+</table>
+<table border="0" cellpadding="2" cellspacing="5" width="100%">
+  <tr>
+    <td class="main3" width="890px"><b>Permissions:</b></td>
+  </tr>
+  <tr>
+    <td class="main">
+      cd peshmarga <br/>
+      chmod +x peshmarga.py<br/>
+      pyhton peshmarga.py<br/>
+    </td>
+  </tr>
+</table>
+
+<table border="0" cellpadding="0" cellspacing="2" width="100%">
+
+  </tr>
+  <tr>
+    <td width="100px" class="main2"><b>site:</b></td><td>http://kurdforums.co/</td>
+  </tr>
+</table>
+
+
+
+</body>
+
+</html>
---
+++ peshmarga.py
@@ -0,0 +1,36 @@
+import base64
+import os
+import webbrowser
+import time
+demosfile=base64.b64decode("Kz09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PSs=")
+infile=base64.b64decode('fC4uLi4uLi4uLi4uLi4uLi5FbmNvZGUgc2hlbGwuLi4uLi4uLi4uLnw=')
+renamderso=base64.b64decode('Ky0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSs=')
+heviegher=base64.b64decode('fC4uLi4uLiBjMGQzZCBieSA0clRpTiBtelVyZSAuLi4uLi58DQo=')
+acersiong=base64.b64decode('Ky0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSs=')
+pengo=base64.b64decode('PDwtPSB3d3cua3VyZGZvcnVtcy5jbyA9LT4+')
+myhert=base64.b64decode('aHR0cDovL2t1cmRmb3J1bXMuY28v')
+print infile
+print renamderso
+print heviegher
+print acersiong
+print pengo
+print ("=====================================================")
+print ("")
+print ("")
+print ("")
+print ("")
+fileshell=raw_input("Enter the file to be encrypted(php,txt):")
+readfiellshell = open(fileshell,'r')
+print ("Please wait Gary read file")
+reagingfileshell = readfiellshell.read()
+time.sleep(5)
+print ("Please wait Gary achieve encryption")
+byartin=base64.b64encode(reagingfileshell)
+time.sleep(5)
+OpenNewFileAndWrite=open("Peshmarga.php",'w')
+OpenNewFileAndWrite.write("%s%s"%('''<?php eval("?>".base64_decode("''',byartin.strip()+'''+")); ?>'''))
+time.sleep(3)
+print ("Encryption has been successfully\n")
+print ("You will find the script's next\n")
+print ('''name The shell:\n=>Peshmarga.php<=''')
+webbrowser.open(myhert)
